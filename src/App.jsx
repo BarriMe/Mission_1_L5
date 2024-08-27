ed45fded67f749a3cc15203619ae67455f274fa3
BarriMe <168780966+BarriMe@users.noreply.github.com>
2024-08-27 16:36:48 +1200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Submission from "./resources/ImgSubmission";
+
+function App() {
+  return (
+    <>
+        <title>Barry Mission 1</title>
+      <Submission />
+    </>
+  );
+}
+
+export default App;
